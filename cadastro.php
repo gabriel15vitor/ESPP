31351202d43ef8315b36a08cea45f929594318e7
GABRIEL VITOR BARBOSA <gabriel.barbosa137@etec.sp.gov.br>
2020-03-21 18:24:43 -0300

    cadastro com dao

--- cadastro.php
+++ cadastro.php
@@ -1,7 +1,6 @@
 <?php 
 
 include_once "banco.php";
-//include_once "db.php";
 include_once "usuario.php";
 
 $database = new Database();
@@ -19,23 +18,18 @@ $estado = $_POST['estado'];
 
 $res = $usuarioDAO->readOne($usuario);
 $num = $res->rowCount();
-/*
-$query = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
-$consulta_user = mysqli_query($conexao, $query);
-$linha = mysqli_fetch_array($consulta_user);
-*/
 
-if (is_null($nome) || is_null($usuario) || is_null($email) || is_null($senha) || is_null($conf_senha) || is_null($cidade) || is_null($estado)) {
-	header('location: index.php/pg_cadastro.php?erro_faltando');
+if (is_null($nome) || is_null($usuario) || is_null($email) || is_null($senha) || is_null($conf_senha)) {
+	header('location: index.php?pagina=cadastro&erro_faltando');
 }elseif ($num > 0) {
-	header('location: index.php/pg_cadastro.php?erro_usuario');
+	header('location: index.php?pagina=cadastro&erro_usuario');
 }elseif ($senha != $conf_senha) {
-	header('location: index.php/pg_cadastro.php?erro_senha');
+	header('location:index.php?pagina=cadastro&erro_senha2');
 }elseif (strlen($senha) <= 8) {
-	header('location: index.php/pg_cadastro.php?erro_senha2');
+	header('location: index.php?pagina=cadastro&erro_senha');
 }else{
 	if ($usuarioDAO->insert($nome, $usuario, $email, $senha)) {
-		header('location:views/pg_login.php');
+		header('location:index.php?pagina=login');
 	}else{
 		echo "falhou";
 	}
